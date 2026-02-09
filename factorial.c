28d90ad14cbc1aa2b31b7502ae1523e9f9d98dc6
Shiva <gshiva.kumar0111@gmail.com>
2026-02-09 17:24:35 +0530

    factorial

---
+++ factorial.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    long fact=1;
+    printf("enter the number:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        fact*=i;
+    }
+    printf("Factorial=%d",fact);
+    return 0;
+}
